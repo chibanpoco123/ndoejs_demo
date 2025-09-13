889e1016b98a692c96a9b063036d084ac9761309
chibanpco123 <hoang26022005sc@gmail.com>
2025-09-13 17:04:56 +0700

    bài tập

---
+++ test.js
@@ -0,0 +1,13 @@
+function hienthitentuoi(name,age) {
+    console.log(`tên của bạn là ${name}`)
+    console.log(`tuổi của bạn là  ${age}`)
+}
+hienthitentuoi("Hoàng",21)
+
+function tinhnamsinh(namsinh){
+    const namHientai = new Date().getFullYear();
+    return namHientai -namsinh
+}
+const namsinh = 2003
+const age =  tinhnamsinh(namsinh)
+hienthitentuoi("Nam", age);
